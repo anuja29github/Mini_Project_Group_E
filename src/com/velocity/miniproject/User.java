e62a248139745505a00064b269d1aaef8bfb3f76
anuja <anujambhilare29@gmail.com>
2023-01-28 17:12:50 +0530

    User and ConnectionProvider classes added

---
+++ src/com/velocity/miniproject/User.java
@@ -0,0 +1,73 @@
+package com.velocity.miniproject;
+
+import java.util.Scanner;
+
+public class User {
+	
+	ConnectionProvider connect = new ConnectionProvider();
+	Quiz q = new Quiz();
+	Scanner sc = new Scanner(System.in);
+
+	public void selectOption(int ch)
+	{
+		switch(ch)
+		{
+		case 1: 
+				String fname = validationF();
+				String lname = validationL();
+				String city = validationC();
+				String mobno = validationMobile();
+				
+				connect.insertUserData(fname, lname, city, mobno);
+				connect.displayQuestion(fname, lname);
+				break;
+				
+		case 2: 
+			System.out.println("-----------------------------List of Student are as below---------------------");
+			connect.studentRecord();
+			q.quizz();
+			break;
+			
+		case 3:
+			System.out.println("\n"+"Enter the first and last name of candidate to search for : ");
+			String f = sc.next();
+			String l = sc.next();
+			connect.searchStudent(f,l);
+			q.quizz();
+			break;
+		case 4: 
+			System.out.println("***********************Thanks for attempting quiz**********************");
+			System.out.println("-----------------------See You Soon------------------------------------");
+			break;
+			
+			default: 
+				System.out.println("Please Choose correct option");
+		}
+	}
+
+	public String validationF() {
+		System.out.println("Please Enter your first name : ");
+		String fname = sc.nextLine();
+		boolean b = fname.matches("[A-Z][a-z]*");
+		if(b!=true)
+		{
+			System.err.println("Please enter valid first name");
+			validationF();
+		}
+		return fname;
+					
+	}
+	public String validationL() {
+		System.out.println("Please Enter your last name : ");
+		String lname = sc.nextLine();
+		
+		boolean b = lname.matches("[A-Z][a-z]*");
+		if(b!=true)
+		{
+			System.err.println("Please enter valid last name"+"\n");
+			validationL();
+		}
+		return lname;
+	}
+
+}
