bb43c303ae16a717b925d19cdd783957d6978701
anuja <anujambhilare29@gmail.com>
2023-01-28 21:06:40 +0530

    methods added

--- src/com/velocity/miniproject/ConnectionProvider.java
+++ src/com/velocity/miniproject/ConnectionProvider.java
@@ -87,5 +87,146 @@ public class ConnectionProvider {
 			e.printStackTrace();
 		}
 	}
+	public void checkAnswer(int que)
+	{
+		try
+		{
+			System.out.println("\n"+"Enter answer (only in a,b,c,d format) : ");
+			String ans = sc.next();
+			if(ans.equals("a") || ans.equals("b") || ans.equals("c") || ans.equals("d"))
+			{
+			con = getConnectionDetails();
+			ps = con.prepareStatement("select answer from questionlist where qno = ?");
+			ps.setInt(1,que);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				String answer = rs.getString(1);
+	
+				if(answer.equals(ans))
+				{
+					count++;
+				}
+			}
+			rs.close();
+			}
+			else
+			{
+				System.err.println("Please enter answer in correct format");
+				checkAnswer(que);
+			}
+			con.close();
+			ps.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void score(int marks, String fname, String lname)
+	{
+		try
+		{
+			con = getConnectionDetails();
+			ps = con.prepareStatement("insert into student (firstName, lastName, score, grade) values(?,?,?,?)");
+
+			ps.setString(1, fname);
+			ps.setString(2, lname);
+			ps.setInt(3, marks );
+			if(marks >= 8 && marks <= 10)
+			{
+				ps.setString(4,"Class A" );
+			}
+			else if(marks >=6 && marks < 8)
+			{
+				ps.setString(4, "Class B");
+			}
+			else if(marks == 5)
+			{
+				ps.setString(4, "Class C");
+			}
+			else
+			{
+				ps.setString(4, "Class D");
+			}
+			
+			int i = ps.executeUpdate();
+			System.out.println("Record added " + i);
+			
+			ps = con.prepareStatement("select score, grade from student where firstName = ? and lastName = ?");
+			ps.setString(1, fname);
+			ps.setString(2, lname);
+			System.out.println("------------------------------------------------------------------");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				System.out.println("Your Score is: "+rs.getInt(1));
+				System.out.println("Your Grade is : "+rs.getString(2));
+			}
+			System.out.println("-------------------------------------------------------------------");
+			qu.quizz();
+			
+			con.close();
+			ps.close();
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void studentRecord()
+	{
+		try
+		{
+			con = getConnectionDetails();
+			ps = con.prepareStatement("select * from student");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				System.out.println("First name : "+rs.getString(2));
+				System.out.println("Last name : "+rs.getString(3));
+				System.out.println("Score : "+rs.getInt(4));
+				System.out.println("Grade : "+rs.getString(5)+"\n");
+			}
+			con.close();
+			ps.close();
+			rs.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void searchStudent(String f, String l)
+	{
+		try
+		{
+			con = getConnectionDetails();
+			ps = con.prepareStatement("select * from student where firstName = ? and lastName = ?");
+			ps.setString(1, f);
+			ps.setString(2, l);
+			System.out.println("-------------------------------------------------------------------");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				System.out.println("First name : "+rs.getString(2));
+				System.out.println("Last name : "+rs.getString(3));
+				System.out.println("Score : "+rs.getInt(4));
+				System.out.println("Grade : "+rs.getString(5)+"\n");
+			}
+			System.out.println("--------------------------------------------------------------------");
+			con.close();
+			ps.close();
+			rs.close();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 
 }
